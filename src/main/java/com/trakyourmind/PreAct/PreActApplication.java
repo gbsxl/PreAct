6cb8a6a0f18bb49e07f8c6e96cb268d1f9dcb54a
gbsxl <gasousaoli@gmail.com>
2025-04-25 16:24:24 -0300

    init-project

---
+++ src/main/java/com/trakyourmind/PreAct/PreActApplication.java
@@ -0,0 +1,13 @@
+package com.trakyourmind.PreAct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreActApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreActApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trakyourmind/PreAct/PreActApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trakyourmind.PreAct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreActApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
